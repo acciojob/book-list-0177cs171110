f08cbfd6eff6e80c8552ef2e915b807eb0d264fa
Rahul Kumar <90969936+0177cs171110@users.noreply.github.com>
2024-03-27 12:46:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,31 @@
-//your JS code here. If required.
-const form = document.querySelector(".form-group");
-      const submit = document.getElementById("submit");
-      const tbody = document.getElementById("book-list");
-
-      function deleteRecod(event) {
-        const deleteButton = event.target;
-        const record = deleteButton.parentNode;
-        record.remove();
-      }
-
-      const addBook = (event) => {
-        event.preventDefault();
-        const obj = {
-          title: form["title"].value,
-          author: form["author"].value,
-          isbn: form["isbn"].value,
-        };
-        form.reset();
-        createNewRecord(obj);
-      };
-
-      function createNewRecord(data) {
-        const row = document.createElement("tr");
-        for (let key in data) {
-          const cell = document.createElement("td");
-          cell.innerText = data[key];
-          row.appendChild(cell);
-        }
-        const clear = document.createElement("td");
-        clear.innerText = "X";
-        clear.className = "delete";
-        clear.addEventListener("click", deleteRecod);
-        row.appendChild(clear);
-        tbody.appendChild(row);
-      }
-      submit.addEventListener("click", addBook);
+document.getElementById("submit").addEventListener("click", function(event) {
+      event.preventDefault();
+      addBookToList();
+    });
+let tbody=document.getElementById("book-list");
+function addBookToList(){
+	let row=tbody.insertRow();
+	tbody.append(row);
+	let td1=row.insertCell(0);
+	td1.innerText=document.getElementById("title").value;
+	let td2=row.insertCell(1);
+	td2.innerText=document.getElementById("author").value;
+	let td3=row.insertCell(2);
+	td3.innerText=document.getElementById("isbn").value;
+	let td4=row.insertCell(3);
+	let newBtn = document.createElement('button');
+	newBtn.className="delete";
+    newBtn.innerText = 'X';
+	row.append(td1);
+	row.append(td2);
+	row.append(td3);
+	row.append(td4);
+	td4.append(newBtn);
+ 
+	var deleteButtons = document.getElementsByClassName("delete");
+      for (var i = 0; i < deleteButtons.length; i++) {
+        deleteButtons[i].addEventListener("click", function(event) {
+          event.target.parentNode.parentNode.remove();
+        });
+      }	
+}
